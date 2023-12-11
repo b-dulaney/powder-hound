b751b6b1ed8087be09841a796927e485320ac9df
b-dulaney <brendanfdulaney@gmail.com>
2023-12-10 17:44:44 -0700

    chore: improved rendering for conditions table

--- app/src/routes/conditions/+page.svelte
+++ app/src/routes/conditions/+page.svelte
@@ -23,12 +23,6 @@
   <div class="flex justify-center mb-4 px-8 md:py-8">
     <input class="input w-2/3 md:w-1/2" title="Input (search)" type="search" placeholder="Search..." bind:value={searchInput} on:input={searchLocations}/>
   </div>
-
-  {#if searchInput && filteredMountains.length === 0}
-    <div class="flex justify-center mb-4">
-      <h3 class="h3">No results found</h3>
-    </div>
-  {:else if filteredMountains.length > 0}
     <div class="table-container">
       <table class="table">
         <thead class="w-full">
@@ -44,71 +38,65 @@
           </tr>
         </thead>
         <tbody>
-          {#each filteredMountains as row, i}
+        {#if searchInput && filteredMountains.length === 0}
           <tr>
-              <td  class="table-cell-fit"><a class="anchor text-primary-500-400-token xl:text-lg" href="/conditions">{row.display_name}</a></td>
-              <td class="hidden capitalize xl:table-cell xl:text-center xl:table-cell-fit">
-                {#if row.location_type === "resort"}
-                <div class="badge variant-ghost-primary">
-                  {row.location_type}
-                </div>
-                {:else}
-                <div class="badge variant-ghost-success">
-                  {row.location_type}
-                </div>
-                {/if}
-              </td>
-              <td class="hidden font-semibold lg:table-cell lg:text-center lg:table-cell-fit">{row.currenttemp}&degF</td>
-              <td class="hidden font-semibold md:table-cell md:text-center md:table-cell-fit">{row.past5daysnowfall}"</td>
-              <td class="text-center font-semibold">{row.past24hoursnowfall}"</td>
-              <td class="text-center font-semibold">{row.next24hoursnowfall}"</td>
-              <td class="hidden font-semibold md:table-cell md:text-center md:table-cell-fit">{row.next72hoursnowfall}"</td>
-              <td class="text-center font-semibold"><button type="button" class="btn btn-icon-sm space-x-0 px-0 py-0 w-[20px]"><i class="fa-regular fa-star"></i></button></td>
-            </tr>
-          {/each}
-        </tbody>
-      </table>
-    </div>
-  {:else if mountainOverviews}
-  <div class="table-container">
-    <table class="table">
-      <thead class="w-full">
-        <tr>
-          <th class="table-cell-fit">Location</th>
-          <th class="hidden xl:table-cell xl:text-center xl:table-cell-fit">Type</th>
-          <th class="hidden lg:table-cell lg:text-center lg:table-cell-fit">Weather</th>
-          <th class="hidden md:table-cell md:text-center md:table-cell-fit">Last 5 Days</th>
-          <th class="text-center table-cell-fit">Last 24H</th>
-          <th class="text-center table-cell-fit">Next 24H</th>
-          <th class="hidden md:table-cell md:text-center md:table-cell-fit">Next 72H</th>
-          <th class="table-cell-fit"></th>
-        </tr>
-      </thead>
-      <tbody>
-        {#each mountainOverviews as row, i}
-        <tr>
-            <td  class="table-cell-fit"><a class="anchor text-primary-500-400-token xl:text-lg" href="/conditions">{row.display_name}</a></td>
-            <td class="hidden capitalize xl:table-cell xl:text-center xl:table-cell-fit">
-              {#if row.location_type === "resort"}
-              <div class="badge variant-ghost-primary">
-                {row.location_type}
-              </div>
-              {:else}
-              <div class="badge variant-ghost-success">
-                {row.location_type}
-              </div>
-              {/if}
+            <td colspan="8">
+            <div class="flex justify-center m-4">
+              <h3 class="h3">No results found</h3>
+            </div>
             </td>
-            <td class="hidden font-semibold lg:table-cell lg:text-center lg:table-cell-fit">{row.currenttemp}&degF</td>
-            <td class="hidden font-semibold md:table-cell md:text-center md:table-cell-fit">{row.past5daysnowfall}"</td>
-            <td class="text-center font-semibold">{row.past24hoursnowfall}"</td>
-            <td class="text-center font-semibold">{row.next24hoursnowfall}"</td>
-            <td class="hidden font-semibold md:table-cell md:text-center md:table-cell-fit">{row.next72hoursnowfall}"</td>
-            <td class="text-center font-semibold"><button type="button" class="btn btn-icon-sm space-x-0 px-0 py-0 w-[20px]"><i class="fa-regular fa-star"></i></button></td>
           </tr>
-        {/each}
-      </tbody>
-    </table>
+
+        {:else}
+          {#if filteredMountains.length > 0}
+            {#each filteredMountains as row, i}
+            <tr>
+                <td  class="table-cell-fit"><a class="anchor text-primary-500-400-token xl:text-lg" href="/conditions">{row.display_name}</a></td>
+                <td class="hidden capitalize xl:table-cell xl:text-center xl:table-cell-fit">
+                  {#if row.location_type === "resort"}
+                  <div class="badge variant-ghost-primary">
+                    {row.location_type}
+                  </div>
+                  {:else}
+                  <div class="badge variant-ghost-success">
+                    {row.location_type}
+                  </div>
+                  {/if}
+                </td>
+                <td class="hidden font-semibold lg:table-cell lg:text-center lg:table-cell-fit">{row.currenttemp}&degF</td>
+                <td class="hidden font-semibold md:table-cell md:text-center md:table-cell-fit">{row.past5daysnowfall}"</td>
+                <td class="text-center font-semibold">{row.past24hoursnowfall}"</td>
+                <td class="text-center font-semibold">{row.next24hoursnowfall}"</td>
+                <td class="hidden font-semibold md:table-cell md:text-center md:table-cell-fit">{row.next72hoursnowfall}"</td>
+                <td class="text-center font-semibold"><button type="button" class="btn btn-icon-sm space-x-0 px-0 py-0 w-[20px]"><i class="fa-regular fa-star"></i></button></td>
+              </tr>
+            {/each}
+          {:else if mountainOverviews}
+            {#each mountainOverviews as row, i}
+            <tr>
+                <td  class="table-cell-fit"><a class="anchor text-primary-500-400-token xl:text-lg" href="/conditions">{row.display_name}</a></td>
+                <td class="hidden capitalize xl:table-cell xl:text-center xl:table-cell-fit">
+                  {#if row.location_type === "resort"}
+                  <div class="badge variant-ghost-primary">
+                    {row.location_type}
+                  </div>
+                  {:else}
+                  <div class="badge variant-ghost-success">
+                    {row.location_type}
+                  </div>
+                  {/if}
+                </td>
+                <td class="hidden font-semibold lg:table-cell lg:text-center lg:table-cell-fit">{row.currenttemp}&degF</td>
+                <td class="hidden font-semibold md:table-cell md:text-center md:table-cell-fit">{row.past5daysnowfall}"</td>
+                <td class="text-center font-semibold">{row.past24hoursnowfall}"</td>
+                <td class="text-center font-semibold">{row.next24hoursnowfall}"</td>
+                <td class="hidden font-semibold md:table-cell md:text-center md:table-cell-fit">{row.next72hoursnowfall}"</td>
+                <td class="text-center font-semibold"><button type="button" class="btn btn-icon-sm space-x-0 px-0 py-0 w-[20px]"><i class="fa-regular fa-star"></i></button></td>
+              </tr>
+            {/each}
+          {/if}
+        {/if}
+          </tbody>
+        </table>
   </div> 
-  {/if}
 </section>
