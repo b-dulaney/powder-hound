5afb01f981ec41b9d69459297188cc3906e51adb
b-dulaney <brendanfdulaney@gmail.com>
2023-12-20 13:48:17 -0700

    chore: clean up TS definitions for route load functions

--- app/src/routes/conditions/[slug]/+page.server.ts
+++ app/src/routes/conditions/[slug]/+page.server.ts
@@ -1,9 +1,10 @@
 import { supabase } from "$lib/supabaseClient.js"
 import type { MountainDetail } from "$lib/supabase.types"
+import type { PageServerLoad } from './$types';
 import { error } from "@sveltejs/kit"
 export const prerender = false;
 
-export async function load({params}) {
+export const load: PageServerLoad = async ({params}) => {
     const { data } = await supabase.from("mountain_detail").select().eq("slug", params.slug).returns<MountainDetail[]>().single()
 
     if(data){
