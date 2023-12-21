02ec82f58a7233d3691fc873ff12872e765f437a
b-dulaney <brendanfduflaney@gmail.com>
2023-12-21 12:07:04 -0700

    chore: update postcss config

--- app/postcss.config.js
+++ app/postcss.config.js
@@ -1,6 +1,6 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
 }
