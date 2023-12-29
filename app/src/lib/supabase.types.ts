f136989697d8f219a1e006ce449f335d5c82b1c0
b-dulaney <brendanfduflaney@gmail.com>
2023-12-29 11:56:25 -0700

    chore: remove old tables and update db types

--- app/src/lib/supabase.types.ts
+++ app/src/lib/supabase.types.ts
@@ -79,14 +79,11 @@ export type Views<T extends keyof Database['public']['Views']> =
 	Database['public']['Views'][T]['Row'];
 
 export type Mountain = Tables<'mountains'>;
-export type SnowAccumulation = Tables<'snow_accumulation'>;
-export type DailyForecast = Tables<'daily_forecasts'>;
-export type WeeklyForecast = Tables<'weekly_forecasts'>;
 export type CaicData = Tables<'caic_data'>;
 export type MountainOverview = Views<'mountain_overview'>;
 export type MountainDetail = Views<'mountain_details'>;
 
-type TableTypes = Mountain | SnowAccumulation | DailyForecast | WeeklyForecast | CaicData;
+type TableTypes = Mountain | CaicData;
 
 export type DbResult<T extends TableTypes> = {
 	data: T[] | null;
