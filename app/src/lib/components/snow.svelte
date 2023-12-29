942547d97181eaaa502a063b452ce65f0c8c07f1
b-dulaney <brendanfdulaney@gmail.com>
2023-12-28 20:52:10 -0700

    chore: update favicon and snow particle styles

--- app/src/lib/components/snow.svelte
+++ app/src/lib/components/snow.svelte
@@ -85,7 +85,7 @@
 <svelte:component
 	this={ParticlesComponent}
 	id="tsparticles"
-	class="absolute w-full {height} {zIndex}"
+	class="absolute pr-5 w-full {height} {zIndex}"
 	options={particlesConfig}
 	on:particlesLoaded={onParticlesLoaded}
 	{particlesInit}
