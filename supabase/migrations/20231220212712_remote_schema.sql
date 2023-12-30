acdc3586e09663c3d5abee56fd1232a9540b7b92
b-dulaney <brendanfdulaney@gmail.com>
2023-12-29 17:32:04 -0700

    chore: squash migrations to single file (#2)
    * chore: add deployment links to readme
    
    * chore: readme formatting
    
    * chore: squash supabase migrations to single file

--- supabase/migrations/20231220212712_remote_schema.sql
+++
@@ -1,9 +0,0 @@
-drop view if exists "public"."hourly_forecast_view";
-
-create or replace view "public"."hourly_forecast" as  SELECT mountains.mountain_id,
-    mountains.display_name,
-    get_hourly_weather_data(mountains.mountain_id) AS hourly_weather_data
-   FROM mountains;
-
-
-
