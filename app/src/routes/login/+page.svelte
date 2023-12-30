4f51fdbf961d795f681f4b3f9f8782499adbf9c5
b-dulaney <brendanfduflaney@gmail.com>
2023-12-29 19:45:25 -0700

    chore: set up vercel adapter, prettier tailwind formatting, vercel env variable

--- app/src/routes/login/+page.svelte
+++ app/src/routes/login/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import GithubIcon from '$lib/components/github-icon.svelte';
 	import GoogleIcon from '$lib/components/google-icon.svelte';
+	import { PUBLIC_VERCEL_ENV } from '$env/static/public';
 	import type { PageData } from '../$types';
 	let isResendDisabled = false;
 	let email = '';
@@ -64,37 +65,39 @@
 	}
 </script>
 
-<div class="flex items-start mt-10 md:mt-20 md:mb-40 justify-center h-full px-4">
+<div class="mt-10 flex h-full items-start justify-center px-4 md:mb-40 md:mt-20">
 	{#if !emailSent}
-		<div class="card p-4 max-w-sm">
+		<div class="card max-w-sm p-4">
 			<div class="card-header text-center">
 				<p class="text-xl font-semibold">Sign in with</p>
 			</div>
-			<div class="mb-4 p-4 flex flex-col gap-2">
-				<button
-					class="btn btn-lg bg-red-500 rounded-md"
-					title="Sign in with Google"
-					on:click={signInWithGoogle}
-				>
-					<span><GoogleIcon /></span>
-					<span>Google</span>
-				</button>
+			<div class="mb-4 flex flex-col gap-2 p-4">
+				{#if PUBLIC_VERCEL_ENV !== 'production'}
+					<button
+						class="btn btn-lg rounded-md bg-red-500"
+						title="Sign in with Google"
+						on:click={signInWithGoogle}
+					>
+						<span><GoogleIcon /></span>
+						<span>Google</span>
+					</button>
+				{/if}
 				<!-- <button class="btn btn-lg variant-outline-primary rounded-md">Sign in with Microsoft</button>
 			<button class="btn btn-lg variant-outline-primary rounded-md">Sign in with Facebook</button> -->
 				<button
-					class="btn btn-lg bg-neutral-900 rounded-md"
+					class="btn btn-lg rounded-md bg-neutral-900"
 					title="Sign in with Github"
 					on:click={signInWithGithub}
 				>
 					<span><GithubIcon /></span>
 					<span>Github</span>
 				</button>
-				<div class="flex justify-center items-center p-2">
+				<div class="flex items-center justify-center p-2">
 					<hr class="w-1/4 px-4" />
 					<span class="px-4">Or</span>
 					<hr class="w-1/4 px-4" />
 				</div>
-				<p class="text-xl font-semibold text-center mb-2">Continue with email</p>
+				<p class="mb-2 text-center text-xl font-semibold">Continue with email</p>
 				<div>
 					<input
 						class="input mb-3 !rounded-md"
@@ -104,33 +107,33 @@
 						placeholder="Email"
 					/>
 					<button
-						class="btn w-full btn-lg variant-filled-tertiary rounded-md"
+						class="variant-filled-tertiary btn btn-lg w-full rounded-md"
 						on:click={signInWithEmail}>Sign In</button
 					>
 				</div>
 			</div>
 		</div>
 	{:else}
-		<div class="card p-4 max-w-sm">
+		<div class="card max-w-sm p-4">
 			<div class="card-header text-center">
-				<p class="text-3xl font-semibold text-center">Check your email</p>
+				<p class="text-center text-3xl font-semibold">Check your email</p>
 			</div>
-			<div class="flex flex-col justify-between items-center gap-4 mb-4 p-4">
+			<div class="mb-4 flex flex-col items-center justify-between gap-4 p-4">
 				<p class="text-center text-xl">
 					We've sent a magic link to <span class="font-semibold text-primary-400">{email}</span>.
 					Follow the link in the email to sign in to your account.
 				</p>
-				<div class="flex justify-center items-center p-2 w-full mt-4">
+				<div class="mt-4 flex w-full items-center justify-center p-2">
 					<hr class="flex-auto px-4" />
 					<span class="px-4">Didn't receive an email?</span>
 					<hr class="flex-auto px-4" />
 				</div>
 				<button
-					class="btn btn-lg variant-filled-tertiary rounded-md"
+					class="variant-filled-tertiary btn btn-lg rounded-md"
 					on:click={resendEmail}
 					disabled={isResendDisabled}>Resend email</button
 				>
-				<a class="anchor !text-surface-400 mt-2" href="/login" data-sveltekit-reload
+				<a class="anchor mt-2 !text-surface-400" href="/login" data-sveltekit-reload
 					>Sign in with another method</a
 				>
 			</div>
