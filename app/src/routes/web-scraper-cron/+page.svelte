911cbcef619dcb305b2913c06e81cd912e4fc53d
b-dulaney <brendanfdulaney@gmail.com>
2024-01-11 01:31:13 -0700

    feature: web scraper api route

---
+++ app/src/routes/web-scraper-cron/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { PageData } from "./$types";
+
+    export let data: PageData
+</script>
+
+<div class="mt-10 flex h-full items-start justify-center px-4 md:mb-40 md:mt-20">
+    <p>{data.status}</p>
+    <p>{data.body.message}</p>
+</div>
