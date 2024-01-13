51a8cc990b517b0fe94c992686b8e6f7800ad428
b-dulaney <brendanfdulaney@gmail.com>
2024-01-12 17:59:00 -0700

    feature: resort lifts/trails conditions

--- app/src/routes/conditions/[slug]/+page.server.ts
+++ app/src/routes/conditions/[slug]/+page.server.ts
@@ -1,8 +1,24 @@
 import { supabase } from '$lib/supabaseClient.js';
-import type { MountainDetail } from '$lib/supabase.types';
+import type { MountainDetail, ResortConditions } from '$lib/supabase.types';
 import type { PageServerLoad } from './$types';
 import { error } from '@sveltejs/kit';
-export const prerender = false;
+
+function countNonNullProperties(resortConditions: ResortConditions): number {
+	const properties: Partial<keyof ResortConditions>[] = [
+		'base_depth',
+		'snow_past_24h',
+		'snow_past_48h',
+		'snow_past_week',
+		'snow_total'
+	];
+	let count = 0;
+	for (const prop of properties) {
+		if (resortConditions[prop] !== null) {
+			count++;
+		}
+	}
+	return count;
+}
 
 export const load: PageServerLoad = async ({ params }) => {
 	const { data } = await supabase
@@ -13,6 +29,20 @@ export const load: PageServerLoad = async ({ params }) => {
 		.single();
 
 	if (data) {
+		if (data.location_type === 'resort') {
+			const { data: resortConditions } = await supabase
+				.from('resort_conditions')
+				.select()
+				.eq('mountain_id', data.mountain_id)
+				.returns<ResortConditions[]>()
+				.single();
+			const gridCols = countNonNullProperties(resortConditions!);
+			return {
+				mountainDetails: data,
+				resortConditions: resortConditions,
+				gridCols: gridCols
+			};
+		}
 		return {
 			mountainDetails: data
 		};
--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,15 +1,13 @@
 import { join } from 'path';
 import type { Config } from 'tailwindcss';
 import forms from '@tailwindcss/forms';
-// 1. Import the Skeleton plugin
 import { skeleton } from '@skeletonlabs/tw-plugin';
 ``;
 const config = {
-	// 2. Opt for dark mode to be handled via the class method
 	darkMode: 'class',
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
-		// 3. Append the path to the Skeleton package
+		'./node_modules/layerchart/**/*.{svelte,js}',
 		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
 	],
 	theme: {},
