af03ebd4c347028e1a3ec489c74e78831933bb60
b-dulaney <brendanfdulaney@gmail.com>
2024-01-24 10:33:59 -0700

    chore: add card component for standard padding/sizing on all cards

--- app/src/routes/conditions/backcountry/[slug]/+page.svelte
+++ app/src/routes/conditions/backcountry/[slug]/+page.svelte
@@ -5,6 +5,7 @@
 	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
 	import type { PageData } from './$types';
 	import BackcountryLayout from './backcountry-layout.svelte';
+	import Card from '$lib/components/card.svelte';
 	export let data: PageData;
 
 	const { backcountryDetails } = data;
@@ -38,150 +39,152 @@
 <section id="forecast-section">
 	<div class="mx-auto w-full max-w-6xl lg:max-w-[90rem] px-4 pt-4 pb-9 lg:pt-6">
 		<div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
-			<div class="card mt-4 w-full md:p-4 xl:p-6">
-				<div class="card-header">
-					<h3 class="h3">Daily Forecast</h3>
-				</div>
-				<div class="flex h-full flex-col justify-evenly">
-					<div class="flex justify-between sm:w-2/3 sm:self-center py-4">
-						<div class="flex flex-col p-4">
-							<p class="text-3xl font-bold">{backcountryDetails.current_temperature}°</p>
-							<p class="text-xl font-semibold">
-								{weatherConditionsMap[backcountryDetails.current_weather]}
-							</p>
-							<div class="flex">
-								<p>
-									High {backcountryDetails.temperature_range[1].high_temp}&deg;
+			<Card>
+				<svelte:fragment slot="header">Daily Forecast</svelte:fragment>
+				<svelte:fragment slot="body">
+					<div class="flex h-full flex-col justify-evenly">
+						<div class="flex justify-between sm:w-2/3 sm:self-center py-4">
+							<div class="flex flex-col p-4">
+								<p class="text-3xl font-bold">{backcountryDetails.current_temperature}°</p>
+								<p class="text-xl font-semibold">
+									{weatherConditionsMap[backcountryDetails.current_weather]}
 								</p>
-								<p class=" mx-2">&middot;</p>
-								<p>
-									Low {backcountryDetails.temperature_range[1].low_temp}&deg;
-								</p>
+								<div class="flex">
+									<p>
+										High {backcountryDetails.temperature_range[1].high_temp}&deg;
+									</p>
+									<p class=" mx-2">&middot;</p>
+									<p>
+										Low {backcountryDetails.temperature_range[1].low_temp}&deg;
+									</p>
+								</div>
+							</div>
+							<div class="flex flex-col items-center justify-center p-8">
+								<WeatherIcon weatherDesc={backcountryDetails.current_weather} size="large" />
 							</div>
 						</div>
-						<div class="flex flex-col items-center justify-center p-8">
-							<WeatherIcon weatherDesc={backcountryDetails.current_weather} size="large" />
-						</div>
-					</div>
-					<div
-						class="flex w-full items-center justify-between px-4 py-2 md:self-center md:py-6 xl:w-11/12"
-					>
-						{#each backcountryDetails.temperature_range as { date, low_temp, high_temp, snowfall }, i (i)}
-							<WeatherForecastSlice
-								{high_temp}
-								{low_temp}
-								weatherDesc={backcountryDetails.daily_weather_conditions[i].daily_weather}
-								{date}
-							/>
-							{#if i < backcountryDetails.temperature_range.length - 1}
-								<hr class="divider-vertical h-20" />
-							{/if}
-						{/each}
-					</div>
-
-					<Accordion class="py-10 lg:hidden">
-						<AccordionItem>
-							<svelte:fragment slot="summary"
-								><p class="text-center">View Hourly Forecast</p></svelte:fragment
-							>
-							<svelte:fragment slot="content">
-								<ul class="list">
-									{#each backcountryDetails.hourly_forecast as { datetime, temp, weather_desc, snowfall, wind_deg_speed }, i (i)}
-										<li class="!rounded-md">
-											<div class="grid w-full grid-cols-6 items-center gap-3 p-2">
-												<p class="text-xs text-surface-400 sm:text-sm">
-													{new Date(datetime).toLocaleTimeString('en-US', {
-														hour: 'numeric',
-														hour12: true,
-														timeZone: 'America/Denver'
-													})}
-												</p>
-												<WeatherIcon
-													datetime={new Date(datetime)}
-													weatherDesc={weather_desc}
-													size="small"
-												/>
-												{#if snowfall > 0}
-													<p class="text-sm font-semibold">{snowfall}"</p>
-												{:else}
-													<p class="text-start text-sm">--</p>
-												{/if}
-												<p class="text-sm">{temp}&deg;</p>
-												<span class="col-span-2">
-													<div class="flex justify-evenly">
-														<p class="text-xs sm:text-sm">
-															{convertWindSpeed(wind_deg_speed)}
-														</p>
-														<p class="text-xs sm:text-sm">
-															<i
-																class="fa-solid fa-location-arrow text-primary-500"
-																style="transform: rotate({parseInt(
-																	wind_deg_speed.split('/')[0],
-																	10
-																) + 140}deg);"
-															></i>&nbsp;
-															{convertWindDirection(wind_deg_speed)}
-														</p>
-													</div>
-												</span>
-											</div>
-										</li>
-										<hr class="mt-1 opacity-60 last:hidden" />
-									{/each}
-								</ul>
-							</svelte:fragment>
-						</AccordionItem>
-					</Accordion>
-				</div>
-			</div>
-			<div class="card mt-4 hidden w-full lg:block lg:p-4 xl:p-6">
-				<div class="card-header">
-					<h3 class="h3">Hourly Forecast</h3>
-				</div>
-				<ul class="list max-h-[500px] overflow-auto px-4 py-6">
-					{#each backcountryDetails.hourly_forecast as { datetime, temp, weather_desc, snowfall, wind_deg_speed }, i (i)}
-						<li class="!rounded-md">
-							<div class="grid w-full grid-cols-6 items-center gap-3 p-2">
-								<p class="text-xs text-surface-400 sm:text-sm">
-									{new Date(datetime).toLocaleTimeString('en-US', {
-										hour: 'numeric',
-										hour12: true,
-										timeZone: 'America/Denver'
-									})}
-								</p>
-								<WeatherIcon
-									datetime={new Date(datetime)}
-									weatherDesc={weather_desc}
-									size="small"
+						<div
+							class="flex w-full items-center justify-between px-4 py-2 md:self-center md:py-6 xl:w-11/12"
+						>
+							{#each backcountryDetails.temperature_range as { date, low_temp, high_temp, snowfall }, i (i)}
+								<WeatherForecastSlice
+									{high_temp}
+									{low_temp}
+									weatherDesc={backcountryDetails.daily_weather_conditions[i].daily_weather}
+									{date}
 								/>
-								{#if snowfall > 0}
-									<p class="text-sm font-semibold">{snowfall}"</p>
-								{:else}
-									<p class="text-start text-sm">--</p>
+								{#if i < backcountryDetails.temperature_range.length - 1}
+									<hr class="divider-vertical h-20" />
 								{/if}
-								<p class="text-sm">{temp}&deg;</p>
-								<span class="col-span-2">
-									<div class="flex justify-evenly">
-										<p class="text-xs sm:text-sm">
-											{convertWindSpeed(wind_deg_speed)}
+							{/each}
+						</div>
+	
+						<Accordion class="pb-12 lg:hidden">
+							<AccordionItem>
+								<svelte:fragment slot="summary"
+									><p class="text-center">View Hourly Forecast</p></svelte:fragment
+								>
+								<svelte:fragment slot="content">
+									<ul class="list">
+										{#each backcountryDetails.hourly_forecast as { datetime, temp, weather_desc, snowfall, wind_deg_speed }, i (i)}
+											<li class="!rounded-md">
+												<div class="grid w-full grid-cols-6 items-center gap-3 p-2">
+													<p class="text-xs text-surface-400 sm:text-sm">
+														{new Date(datetime).toLocaleTimeString('en-US', {
+															hour: 'numeric',
+															hour12: true,
+															timeZone: 'America/Denver'
+														})}
+													</p>
+													<WeatherIcon
+														datetime={new Date(datetime)}
+														weatherDesc={weather_desc}
+														size="small"
+													/>
+													{#if snowfall > 0}
+														<p class="text-sm font-semibold">{snowfall}"</p>
+													{:else}
+														<p class="text-start text-sm">--</p>
+													{/if}
+													<p class="text-sm">{temp}&deg;</p>
+													<span class="col-span-2">
+														<div class="flex justify-evenly">
+															<p class="text-xs sm:text-sm">
+																{convertWindSpeed(wind_deg_speed)}
+															</p>
+															<p class="text-xs sm:text-sm">
+																<i
+																	class="fa-solid fa-location-arrow text-primary-500"
+																	style="transform: rotate({parseInt(
+																		wind_deg_speed.split('/')[0],
+																		10
+																	) + 140}deg);"
+																></i>&nbsp;
+																{convertWindDirection(wind_deg_speed)}
+															</p>
+														</div>
+													</span>
+												</div>
+											</li>
+											<hr class="mt-1 opacity-60 last:hidden" />
+										{/each}
+									</ul>
+								</svelte:fragment>
+							</AccordionItem>
+						</Accordion>
+					</div>
+				</svelte:fragment>
+			</Card>
+			<div class="hidden lg:block">
+				<Card>
+					<svelte:fragment slot="header">Hourly Forecast</svelte:fragment>
+					<svelte:fragment slot="body">
+						<ul class="list max-h-[500px] overflow-auto px-4 py-6">
+							{#each backcountryDetails.hourly_forecast as { datetime, temp, weather_desc, snowfall, wind_deg_speed }, i (i)}
+								<li class="!rounded-md">
+									<div class="grid w-full grid-cols-6 items-center gap-3 p-2">
+										<p class="text-xs text-surface-400 sm:text-sm">
+											{new Date(datetime).toLocaleTimeString('en-US', {
+												hour: 'numeric',
+												hour12: true,
+												timeZone: 'America/Denver'
+											})}
 										</p>
-										<p class="text-xs sm:text-sm">
-											<i
-												class="fa-solid fa-location-arrow text-primary-500"
-												style="transform: rotate({parseInt(wind_deg_speed.split('/')[0], 10) +
-													140}deg);"
-											></i>&nbsp;
-											{convertWindDirection(wind_deg_speed)}
-										</p>
+										<WeatherIcon
+											datetime={new Date(datetime)}
+											weatherDesc={weather_desc}
+											size="small"
+										/>
+										{#if snowfall > 0}
+											<p class="text-sm font-semibold">{snowfall}"</p>
+										{:else}
+											<p class="text-start text-sm">--</p>
+										{/if}
+										<p class="text-sm">{temp}&deg;</p>
+										<span class="col-span-2">
+											<div class="flex justify-evenly">
+												<p class="text-xs sm:text-sm">
+													{convertWindSpeed(wind_deg_speed)}
+												</p>
+												<p class="text-xs sm:text-sm">
+													<i
+														class="fa-solid fa-location-arrow text-primary-500"
+														style="transform: rotate({parseInt(wind_deg_speed.split('/')[0], 10) +
+															140}deg);"
+													></i>&nbsp;
+													{convertWindDirection(wind_deg_speed)}
+												</p>
+											</div>
+										</span>
 									</div>
-								</span>
-							</div>
-						</li>
-						<hr class="mt-1 opacity-60 last:hidden" />
-					{/each}
-				</ul>
+								</li>
+								<hr class="mt-1 opacity-60 last:hidden" />
+							{/each}
+						</ul>
+					</svelte:fragment>
+				</Card>
+
 			</div>
 		</div>
 	</div>
 </section>
-
--- app/src/routes/conditions/backcountry/[slug]/backcountry-layout.svelte
+++ app/src/routes/conditions/backcountry/[slug]/backcountry-layout.svelte
@@ -5,6 +5,7 @@
 	import dayjs from "dayjs";
 	import { Axis, Bars, Chart, Highlight, RectClipPath, Svg, Tooltip, TooltipItem } from "layerchart";
 	import AvalancheDangerIcon from "$lib/components/avalanche-danger-icon.svelte";
+	import Card from "$lib/components/card.svelte";
     export let backcountryDetails: BackcountryDetail;
 
 </script>
@@ -55,133 +56,131 @@
 
 <section id="recent-and-upcoming-snowfall">
 	<div class="mx-auto w-full max-w-6xl lg:max-w-[90rem] px-4 pt-4">
-			<div class="card mt-4 w-full md:p-4 xl:p-6">
-				<div class="card-header">
-					<h3 class="h3">Snow Report</h3>
-				</div>
+			<Card>
+				<svelte:fragment slot="header">Snow Report</svelte:fragment>
+				<svelte:fragment slot="body">
 				<div class="grid grid-cols-1 lg:grid-cols-2">
-				<div class="my-6 flex w-full flex-col items-center">
-					<p class="my-2 text-xl font-bold">Past Week</p>
-					<div class="flex w-full items-center justify-center">
-						<hr class="w-1/4 !border-slate-700 px-2" />
-						<p class="px-6 text-xl">
-							{backcountryDetails.snow_past_7d < 1 && backcountryDetails.snow_past_7d > 0
-								? '< 1'
-								: backcountryDetails.snow_past_7d}"
-						</p>
-						<hr class="w-1/4 !border-slate-700 px-2" />
-					</div>
-					<div class="flex justify-center items-center w-full p-8">
-						<div class="h-[400px]  w-[400px]">
-							<Chart
-							ssr
-							data={backcountryDetails.previous_snowfall_totals}
-							x="date"
-							xScale={scaleBand().domain(backcountryDetails.upcoming_snowfall_totals.map((d) => d.date)).paddingInner(0.2).paddingOuter(0.3)}
-							y="snowfall_total"
-							yDomain={[0, 18]}
-							yNice
-							padding={{ left: 24, bottom: 36 }}
-							tooltip={{ mode: "band" }}
-						  >
-							<Svg>
-							  <Axis placement="left" rule grid={{ style: "stroke-dasharray: 4" }} labelProps={{class: "text-sm md:text-lg tracking-widest fill-surface-50 stroke-surface-50", dx: -15}} tickSize={10} format={(d) => `${d}''`}
-							  />
-							  <Axis
-								placement="bottom"
+					<div class="my-6 flex w-full flex-col items-center">
+						<p class="my-2 text-xl font-bold">Past Week</p>
+						<div class="flex w-full items-center justify-center">
+							<hr class="w-1/4 !border-slate-700 px-2" />
+							<p class="px-6 text-xl">
+								{backcountryDetails.snow_past_7d < 1 && backcountryDetails.snow_past_7d > 0
+									? '< 1'
+									: backcountryDetails.snow_past_7d}"
+							</p>
+							<hr class="w-1/4 !border-slate-700 px-2" />
+						</div>
+						<div class="flex justify-center items-center w-full p-8">
+							<div class="h-[400px]  w-[400px]">
+								<Chart
+								ssr
+								data={backcountryDetails.previous_snowfall_totals}
+								x="date"
+								xScale={scaleBand().domain(backcountryDetails.upcoming_snowfall_totals.map((d) => d.date)).paddingInner(0.2).paddingOuter(0.3)}
+								y="snowfall_total"
+								yDomain={[0, 18]}
+								yNice
+								padding={{ left: 24, bottom: 36 }}
+								tooltip={{ mode: "band" }}
+							>
+								<Svg>
+								<Axis placement="left" rule grid={{ style: "stroke-dasharray: 4" }} labelProps={{class: "text-sm md:text-lg tracking-widest fill-surface-50 stroke-surface-50", dx: -15}} tickSize={10} format={(d) => `${d}''`}
+								/>
+								<Axis
+									placement="bottom"
+									
+									labelProps={{rotate: 315,
+										textAnchor: "end",
+										class: "text-sm md:text-lg tracking-widest fill-surface-50 stroke-surface-50", dy: 20,}}
+									tickSize={0}
+									format={(d) => formatDate(d)}
+								/>
+								<Bars radius={8} strokeWidth={2} class="fill-primary-600 stroke-primary-400 transition-colors" />
+								<Highlight area>
+									<svelte:fragment slot="area" let:area>
+									<RectClipPath
+										x={area.x}
+										y={area.y}
+										width={area.width}
+										height={area.height}
+										spring
+									>
+										<Bars radius={8} strokeWidth={2} class="fill-primary-400 stroke-primary-300" />
+									</RectClipPath>
+									</svelte:fragment>
+								</Highlight>
+								</Svg>
 								
-								labelProps={{rotate: 315,
-									textAnchor: "end",
+								<Tooltip header={(data) => dayjs(data.date).format('MMM DD YYYY')} let:data>
+									<TooltipItem label="Snowfall (in)" value={data.snowfall_total} />
+								</Tooltip>
+							</Chart>
+							</div>
+						</div>
+					</div>
+
+					<div class="my-6 flex w-full flex-col items-center">
+						<p class="my-2 text-xl font-bold">Next 72 Hours</p>
+						<div class="flex w-full items-center justify-center">
+							<hr class="w-1/4 !border-slate-700 px-2" />
+							<p class="px-6 text-xl">
+								{backcountryDetails.snow_next_72h < 1 && backcountryDetails.snow_next_72h > 0
+									? '< 1'
+									: backcountryDetails.snow_next_72h}"
+							</p>
+							<hr class="w-1/4 !border-slate-700 px-2" />
+						</div>
+						<div class="flex justify-center items-center w-full p-8">
+							<div class="h-[400px] w-[400px]">
+								<Chart
+								ssr
+								data={backcountryDetails.upcoming_snowfall_totals}
+								x="date"
+								xScale={scaleBand().domain(backcountryDetails.upcoming_snowfall_totals.map((d) => d.date)).paddingInner(0.4).paddingOuter(0.4)}
+								y="snowfall_total"
+								yDomain={[0, 18]}
+								yNice
+								padding={{ left: 24, bottom: 36 }}
+								tooltip={{ mode: "band" }}
+							>
+							<Svg>
+								<Axis placement="left" rule grid={{ style: "stroke-dasharray: 4", }} labelProps={{class: "text-sm md:text-lg tracking-widest fill-surface-50 stroke-surface-50", dx: -15}} tickSize={10} format={(d) => `${d}''`}
+								/>
+								<Axis
+								placement="bottom"
+								labelProps={{
 									class: "text-sm md:text-lg tracking-widest fill-surface-50 stroke-surface-50", dy: 20,}}
 								tickSize={0}
 								format={(d) => formatDate(d)}
-							  />
-							  <Bars radius={8} strokeWidth={2} class="fill-primary-600 stroke-primary-400 transition-colors" />
-							  <Highlight area>
+								/>
+								<Bars radius={8} strokeWidth={2} class="fill-primary-600 stroke-primary-400 transition-colors" />
+								<Highlight area>
 								<svelte:fragment slot="area" let:area>
-								  <RectClipPath
+									<RectClipPath
 									x={area.x}
 									y={area.y}
 									width={area.width}
 									height={area.height}
 									spring
-								  >
+									>
 									<Bars radius={8} strokeWidth={2} class="fill-primary-400 stroke-primary-300" />
-								  </RectClipPath>
+									</RectClipPath>
 								</svelte:fragment>
-							  </Highlight>
+								</Highlight>
 							</Svg>
-							
-							<Tooltip header={(data) => dayjs(data.date).format('MMM DD YYYY')} let:data>
-								<TooltipItem label="Snowfall (in)" value={data.snowfall_total} />
-							  </Tooltip>
-						  </Chart>
-						  </div>
+								
+								<Tooltip header={(data) => dayjs(data.date).format('MMM DD YYYY')} let:data>
+									<TooltipItem label="Snowfall (in)" value={data.snowfall_total} />
+								</Tooltip>
+							</Chart>
+							</div>
 						</div>
-				</div>
-
-
-
-				<div class="my-6 flex w-full flex-col items-center">
-					<p class="my-2 text-xl font-bold">Next 72 Hours</p>
-					<div class="flex w-full items-center justify-center">
-						<hr class="w-1/4 !border-slate-700 px-2" />
-						<p class="px-6 text-xl">
-							{backcountryDetails.snow_next_72h < 1 && backcountryDetails.snow_next_72h > 0
-								? '< 1'
-								: backcountryDetails.snow_next_72h}"
-						</p>
-						<hr class="w-1/4 !border-slate-700 px-2" />
 					</div>
-					<div class="flex justify-center items-center w-full p-8">
-						<div class="h-[400px] w-[400px]">
-							<Chart
-							ssr
-							data={backcountryDetails.upcoming_snowfall_totals}
-							x="date"
-							xScale={scaleBand().domain(backcountryDetails.upcoming_snowfall_totals.map((d) => d.date)).paddingInner(0.4).paddingOuter(0.4)}
-							y="snowfall_total"
-							yDomain={[0, 18]}
-							yNice
-							padding={{ left: 24, bottom: 36 }}
-							tooltip={{ mode: "band" }}
-						  >
-						  <Svg>
-							<Axis placement="left" rule grid={{ style: "stroke-dasharray: 4", }} labelProps={{class: "text-sm md:text-lg tracking-widest fill-surface-50 stroke-surface-50", dx: -15}} tickSize={10} format={(d) => `${d}''`}
-							/>
-							<Axis
-							  placement="bottom"
-							  labelProps={{
-								  class: "text-sm md:text-lg tracking-widest fill-surface-50 stroke-surface-50", dy: 20,}}
-							  tickSize={0}
-							  format={(d) => formatDate(d)}
-							/>
-							<Bars radius={8} strokeWidth={2} class="fill-primary-600 stroke-primary-400 transition-colors" />
-							<Highlight area>
-							  <svelte:fragment slot="area" let:area>
-								<RectClipPath
-								  x={area.x}
-								  y={area.y}
-								  width={area.width}
-								  height={area.height}
-								  spring
-								>
-								  <Bars radius={8} strokeWidth={2} class="fill-primary-400 stroke-primary-300" />
-								</RectClipPath>
-							  </svelte:fragment>
-							</Highlight>
-						  </Svg>
-							
-							<Tooltip header={(data) => dayjs(data.date).format('MMM DD YYYY')} let:data>
-								<TooltipItem label="Snowfall (in)" value={data.snowfall_total} />
-							  </Tooltip>
-						  </Chart>
-						  </div>
-						  </div>
-				</div>
 
 
 				</div>
-			</div>
+				</svelte:fragment>
+			</Card>
 	</div>
 </section>
