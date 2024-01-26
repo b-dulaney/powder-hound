f5c240c6923322a385ea55ebeee116f770380df3
b-dulaney <brendanfdulaney@gmail.com>
2024-01-25 22:11:42 -0700

    feature: ssr enhancements for login/signup form

--- app/src/routes/login/+page.svelte
+++ app/src/routes/login/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import Login from '$lib/components/login.svelte';
 	import type { PageData } from '../$types';
+	import type { ActionData } from './$types';
 
+	export let form: ActionData;
 	export let data: PageData;
 	const { supabase } = data;
 
 </script>
 
-<div class="mt-10 flex h-full items-start justify-center px-4 md:mb-40 md:mt-20">
-	<Login action="login" supabase={supabase} />
+<div class="mt-10 flex h-full items-start justify-center px-4">
+	<Login action="login" supabase={supabase} form={form} />
 </div>
