ac7396ab7a2e974cab58a6f65d4904a85a04a2fc
b-dulaney <brendanfdulaney@gmail.com>
2024-02-01 10:45:00 -0700

    fix: increase edge function timeout

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			maxDuration: 120,
+			maxDuration: 300,
 			images: {
 				sizes: [128, 256, 480, 640, 750, 828, 1080, 1200, 1920],
 				domains: [],
