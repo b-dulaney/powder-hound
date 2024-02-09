aa02f37ec91736f3ff3c9dc812ac80e17c80b01f
b-dulaney <brendanfdulaney@gmail.com>
2024-02-09 16:36:31 -0700

    fix: move prerender declaration to layout.server.ts

--- app/src/routes/+layout.server.ts
+++ app/src/routes/+layout.server.ts
@@ -1,5 +1,7 @@
 import type { LayoutServerLoad } from './$types';
 
+export const prerender = 'auto';
+
 export const load: LayoutServerLoad = async ({ locals: { getSession } }) => {
 	return {
 		session: await getSession()
