2adf4fd31c1d984648b9bf3e30d3f3ddf8c005e0
b-dulaney <brendanfdulaney@gmail.com>
2024-02-09 17:13:55 -0700

    fix: disable auto prerenders

--- app/src/routes/+layout.server.ts
+++ app/src/routes/+layout.server.ts
@@ -1,7 +1,5 @@
 import type { LayoutServerLoad } from './$types';
 
-export const prerender = 'auto';
-
 export const load: LayoutServerLoad = async ({ locals: { getSession } }) => {
 	return {
 		session: await getSession()
--- app/src/routes/signup/+page.server.ts
+++ app/src/routes/signup/+page.server.ts
@@ -1,8 +1,6 @@
 import { PUBLIC_SITE_URL } from '$env/static/public';
 import { fail, redirect } from '@sveltejs/kit';
 
-export const prerender = false;
-
 export const actions = {
 	login: async (event) => {
 		const { supabase } = event.locals;
