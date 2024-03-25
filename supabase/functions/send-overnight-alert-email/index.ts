082b643959705eb0d66b648bae6cca50c7bb4442
b-dulaney <brendanfdulaney@gmail.com>
2024-03-25 06:09:58 -0600

    chore: temporarily remove overnight snowfall from email alert

--- supabase/functions/send-overnight-alert-email/index.ts
+++ supabase/functions/send-overnight-alert-email/index.ts
@@ -62,7 +62,7 @@ function buildAlertsTable(alerts: Alert[]): string {
                               <tr>
                                 <td align="center" style="font-size:0px;padding:10px 25px 10px 25px;word-break:break-word;">
                                   <div style="font-family:Open Sans,Roboto,Arial,sans-serif;font-size:18px;line-height:1.7;text-align:center;color:#1A1C26;">${
-      alert.snow_overnight ?? alert.snow_past_24h ??
+      alert.snow_past_24h ??
         alert.backcountry_snow_past_24h
     }"<br></div>
                                 </td>
