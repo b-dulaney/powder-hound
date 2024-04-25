c4108894b317236ca3a2a221b25cf06e59fde9c1
b-dulaney <brendanfdulaney@gmail.com>
2024-04-25 10:30:11 -0600

    chore: bump chromium-min and puppeteer-core versions to 123.0.1 and 22.6.0

--- app/src/routes/api/avalanche-forecast-cron/+server.ts
+++ app/src/routes/api/avalanche-forecast-cron/+server.ts
@@ -50,7 +50,7 @@ async function scrapeAvalancheForecast(mountain: MountainCoordinates): Promise<A
 		args: chromium.args,
 		defaultViewport: chromium.defaultViewport,
 		executablePath: await chromium.executablePath(
-			'https://github.com/Sparticuz/chromium/releases/download/v110.0.1/chromium-v110.0.1-pack.tar'
+			'https://github.com/Sparticuz/chromium/releases/download/v123.0.1/chromium-v123.0.1-pack.tar'
 		),
 		headless: chromium.headless,
 		ignoreHTTPSErrors: true
