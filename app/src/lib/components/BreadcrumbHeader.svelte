b41b5df6ad07502f4b788cf4cedb4a57585fff79
b-dulaney <brendanfdulaney@gmail.com>
2024-04-25 13:14:44 -0600

    chore: add BreadcrumbHeader and SectionContainer components

---
+++ app/src/lib/components/BreadcrumbHeader.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let title: string;
+</script>
+
+<section id="header-section" class="mx-auto w-full pt-4 md:px-4 lg:max-w-5xl lg:pt-8">
+	<ol class="breadcrumb px-4 pb-5 md:px-0 lg:text-lg">
+		<li class="crumb"><a class="anchor !text-surface-300" href="/">Home</a></li>
+		<li class="crumb-separator" aria-hidden>&rsaquo;</li>
+		<li class="capitalize text-surface-300">{title}</li>
+	</ol>
+	<div class="flex justify-center lg:mb-4">
+		<h1 class="h2 capitalize">{title}</h1>
+	</div>
+</section>
--- app/src/routes/settings/+page.svelte
+++ app/src/routes/settings/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import BreadcrumbHeader from '../../lib/components/BreadcrumbHeader.svelte';
+
 	import type { PageData } from './$types';
 	import { invalidateAll } from '$app/navigation';
 	import Card from '$lib/components/card.svelte';
+	import SectionContainer from '$lib/components/SectionContainer.svelte';
 	export let data: PageData;
 	const { profile } = data;
 	import dayjs from 'dayjs';
@@ -78,76 +81,63 @@
 	<meta property="og:image:height" content="630" />
 </svelte:head>
 
-<section id="profile">
-	<div class="mx-auto w-full pb-8 pt-4 md:px-4 lg:max-w-5xl lg:pt-8">
-		<ol class="breadcrumb px-4 pb-5 md:px-0 lg:text-lg">
-			<li class="crumb"><a class="anchor !text-surface-300" href="/">Home</a></li>
-			<li class="crumb-separator" aria-hidden>&rsaquo;</li>
-			<li class="text-surface-300">Settings</li>
-		</ol>
-		<h1 class="h1 text-center !text-3xl">Settings</h1>
-		<div class="flex max-h-full w-full justify-center p-4 md:px-0">
-			<Card showHeader={false}>
-				<svelte:fragment slot="body">
-					<div class="flex w-full flex-col p-4">
-						<h4 class="h4">Profile Info</h4>
-						<hr class="mb-4 mt-1 w-full opacity-80" />
-						<div class="flex w-full flex-col gap-4 pb-8">
+<BreadcrumbHeader title="Settings" />
+
+<SectionContainer id="settings">
+	<div class="flex w-full items-center justify-center">
+		<Card showHeader={false}>
+			<svelte:fragment slot="body">
+				<div class="flex w-full flex-col p-4 md:p-6">
+					<h2 class="h4">Profile Info</h2>
+					<hr class="mb-4 mt-1 w-full opacity-80" />
+					<div class="flex w-full flex-col gap-4 pb-8">
+						<input id="userId" name="userId" value={profile?.user_id} hidden aria-hidden readonly />
+						<label for="email" class="label max-w-sm">
+							<span>Email</span>
 							<input
-								id="userId"
-								name="userId"
-								value={profile?.user_id}
-								hidden
-								aria-hidden
+								type="email"
+								id="email"
+								name="email"
+								class="input"
+								value={profile?.email}
 								readonly
 							/>
-							<label for="email" class="label max-w-sm">
-								<span>Email</span>
-								<input
-									type="email"
-									id="email"
-									name="email"
-									class="input"
-									value={profile?.email}
-									readonly
-								/>
-							</label>
-							<label for="createdAt" class="label max-w-sm">
-								<span>Account created</span>
-								<input
-									type="text"
-									id="createdAt"
-									name="createdAt"
-									class="input"
-									value={dayjs(profile?.created_at).format('MMMM D, YYYY')}
-									readonly
-								/>
-							</label>
-						</div>
-						<h4 class="h4">Manage Data</h4>
-						<hr class="mb-4 mt-1 w-full opacity-80" />
-						<div class="flex flex-col gap-2">
-							<p class="py-2 text-sm">
-								Unsubscribe from all PowderHound email alerts. You will still be able to receive
-								email login links.
-							</p>
-							<button
-								class="variant-filled-surface btn btn-sm w-[180px] !text-primary-400"
-								on:click={handleUnsubscribe}
-								disabled={loading}>Unsubscribe from alerts</button
-							>
-							<p class="py-2 text-sm">
-								Delete your account and all associated data. This action cannot be undone.
-							</p>
-							<button
-								class="variant-ghost-primary btn btn-sm w-[150px]"
-								on:click={handleDeleteAccount}
-								disabled={loading}>Delete account</button
-							>
-						</div>
+						</label>
+						<label for="createdAt" class="label max-w-sm">
+							<span>Account created</span>
+							<input
+								type="text"
+								id="createdAt"
+								name="createdAt"
+								class="input"
+								value={dayjs(profile?.created_at).format('MMMM D, YYYY')}
+								readonly
+							/>
+						</label>
 					</div>
-				</svelte:fragment>
-			</Card>
-		</div>
+					<h2 class="h4">Manage Data</h2>
+					<hr class="mb-4 mt-1 w-full opacity-80" />
+					<div class="flex flex-col gap-2">
+						<p class="py-2 text-sm">
+							Unsubscribe from all PowderHound email alerts. You will still be able to receive email
+							login links.
+						</p>
+						<button
+							class="variant-filled-surface btn btn-sm w-[180px] !text-primary-400"
+							on:click={handleUnsubscribe}
+							disabled={loading}>Unsubscribe from alerts</button
+						>
+						<p class="py-2 text-sm">
+							Delete your account and all associated data. This action cannot be undone.
+						</p>
+						<button
+							class="variant-ghost-primary btn btn-sm w-[150px]"
+							on:click={handleDeleteAccount}
+							disabled={loading}>Delete account</button
+						>
+					</div>
+				</div>
+			</svelte:fragment>
+		</Card>
 	</div>
-</section>
+</SectionContainer>
