4e31687ceb563e8e6aa81995730f4f93418030a8
b-dulaney <brendanfdulaney@gmail.com>
2024-05-01 15:13:23 -0600

    feature: updated breadcrumb UI

--- app/src/lib/components/BreadcrumbHeader.svelte
+++ app/src/lib/components/BreadcrumbHeader.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
+	import { Breadcrumb, BreadcrumbItem } from 'flowbite-svelte';
+
 	export let title: string;
-	export let maxWidth: string = '5xl';
 </script>
 
-<section id="header-section" class={`mx-auto w-full pt-4 md:px-4 lg:max-w-${maxWidth} lg:pt-8`}>
-	<ol class="breadcrumb px-4 pb-5 md:px-0 lg:text-lg">
-		<li class="crumb"><a class="anchor !text-surface-300" href="/">Home</a></li>
-		<li class="crumb-separator" aria-hidden>&rsaquo;</li>
-		<li class="capitalize text-surface-300">{title}</li>
-	</ol>
-	<div class="flex justify-center lg:mb-4">
-		<h1 class="h2 capitalize">{title}</h1>
-	</div>
-</section>
+<Breadcrumb
+	aria-label="Navigation breadcrumbs"
+	solid
+	class="rounded-none border-x-0 border-t-0"
+	olClass="inline-flex items-center space-x-1 mx-auto w-full max-w-screen-xl"
+>
+	<BreadcrumbItem href="/" home>Home</BreadcrumbItem>
+	<BreadcrumbItem>{title}</BreadcrumbItem>
+</Breadcrumb>
